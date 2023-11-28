2a20128b27a8b3d5ff18382ff3f05c4569ec58fb
franticboDev <it.franticbo@gmail.com>
2023-11-28 09:11:15 +0200

    Added description

--- main.py
+++ main.py
@@ -18,11 +18,11 @@ async def on_ready():
     except Exception as e:
         print(e)
 
-@bot.tree.command(name="ping")
+@bot.tree.command(name="ping", description="Pings me.")
 async def ping(interaction: discord.Interaction):
     await interaction.response.send_message('Pong 🏓! Api latency: {0}s'.format(round(bot.latency, 1)), ephemeral=True)
 
-@bot.tree.command(name="say")
+@bot.tree.command(name="say", description="I will send a message.")
 @app_commands.describe(thing_to_say = "What should I say?")
 async def say(interaction: discord.Interaction, thing_to_say: str):
     await interaction.response.send_message("sent message", ephemeral=True)
